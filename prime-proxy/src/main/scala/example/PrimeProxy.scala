cc54b9b22c90a7c708f22cbc558cff507ab64f75
Christian Ravn <christian.ravnen@gmail.com>
2021-06-23 17:49:17 +0200

    feat: prime-proxy calls prime-service

--- prime-proxy/src/main/scala/example/PrimeProxy.scala
+++ prime-proxy/src/main/scala/example/PrimeProxy.scala
@@ -3,6 +3,7 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.stream.scaladsl._
 import akka.util.ByteString
 import akka.http.scaladsl.Http
+import akka.grpc.GrpcClientSettings
 import akka.stream.scaladsl.Source
 import akka.http.scaladsl.model.{HttpEntity, ContentTypes}
 import akka.http.scaladsl.server.Directives._
@@ -21,15 +22,22 @@ object PrimeProxy {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
+    val clientSettings =
+      GrpcClientSettings.connectToServiceAt("127.0.0.1", 8081).withTls(false)
+    val client = PrimesClient.apply(clientSettings)
+
     val route =
       pathPrefix("prime") {
-        path(IntNumber) { int =>
+        path(LongNumber) { int =>
           get {
             complete(
               HttpEntity(
                 ContentTypes.`text/plain(UTF-8)`,
                 // transform each number to a chunk of bytes
-                getPrimeNumbers(int).map(n => ByteString(s"$n\n"))
+                client
+                  .primeStream(Request(int))
+                  .map(response => response.result)
+                  .map(n => ByteString(s"$n\n"))
               )
             )
           }
@@ -41,7 +49,9 @@ object PrimeProxy {
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+      .onComplete(_ => {
+        client.close(); system.terminate()
+      }) // and shutdown when done
   }
 
 }
